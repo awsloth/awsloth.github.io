6c3e3420b1c4b78837696f359c87e6033334ddea
awsloth <awsloth@gmail.com>
2026-04-12 18:40:42 +0100

    Fix sorting

--- assets/nhlstandings.js
+++ assets/nhlstandings.js
@@ -1,3 +1,12 @@
+function sortFunc(a, b) {
+
+    return b[1][0] - a[1][0] || (a[1][1] + a[1][2] + a[1][3] + a[1][4]) - (b[1][1] + b[1][2] + b[1][3] + b[1][4]) || b[1][1] - a[1][1];
+}
+
+function addGames(a, b) {
+    return [a[0] + b[0], a[1] + b[1], a[2] + b[2], a[3] + b[3], a[4] + b[4]];
+}
+
 class Standings {
   // detPoints : game -> (abbrev: points, abbrev: points)
   constructor(detPoints, tableID) {
@@ -8,13 +17,13 @@ class Standings {
       for (let i = 0; i < data.length; i++) {
           var game = detPoints(data[i]);
           if (game[0][0] in this.standings) {
-              this.standings[game[0][0]] = this.standings[game[0][0]] + game[0][1];
+              this.standings[game[0][0]] = addGames(this.standings[game[0][0]], game[0][1]);
           } else {
               this.standings[game[0][0]] = game[0][1];
           }
 
           if (game[1][0] in this.standings) {
-              this.standings[game[1][0]] = this.standings[game[1][0]] + game[1][1];
+              this.standings[game[1][0]] = addGames(this.standings[game[1][0]], game[1][1]);
           } else {
               this.standings[game[1][0]] = game[1][1];
           }
@@ -23,7 +32,7 @@ class Standings {
 
   showStandings() {
     // sort standings
-    const sortedDict = Object.fromEntries(Object.entries(this.standings).sort(([,a],[,b]) => b - a));
+    const sortedDict = Object.fromEntries(Object.entries(this.standings).sort(sortFunc));
 
     var table = document.getElementById(this.tableID);
 
@@ -34,13 +43,13 @@ class Standings {
         var cell2 = row.insertCell(1);
 
         cell1.innerHTML = key;
-        cell2.innerHTML = this.standings[key];
+        cell2.innerHTML = this.standings[key][0];
     }
   }
 
   updateStandings() {
     // sort standings
-    const sortedDict = Object.fromEntries(Object.entries(this.standings).sort(([,a],[,b]) => b - a));
+    const sortedDict = Object.fromEntries(Object.entries(this.standings).sort(sortFunc));
 
     var table = document.getElementById(this.tableID);
 
@@ -52,7 +61,7 @@ class Standings {
         var cell2 = row.cells[1];
 
         cell1.innerHTML = key;
-        cell2.innerHTML = this.standings[key];
+        cell2.innerHTML = this.standings[key][0];
         i++;
     }
   }
@@ -60,39 +69,7 @@ class Standings {
 
 import data from "./nhldata.json" with {type: "json"};
 
-function detGame(x) {
-    const RW = 2;
-    const OTW = 2;
-    const OTL = 1;
-    const L = 0;
-
-    var away = x.awayTeam;
-    var home = x.homeTeam;
-
-    var away_abbrev = away.abbrev;
-    var home_abbrev = home.abbrev;
-
-    var away_score = away.score;
-    var home_score = home.score;
-
-    var wintype = x.gameOutcome.lastPeriodType;
-
-    if (wintype == "REG") {
-        if (away_score > home_score) {
-            return [[away_abbrev, RW], [home_abbrev, L]];
-        } else {
-            return [[home_abbrev, RW], [away_abbrev, L]];
-        }
-    } else {
-        if (away_score > home_score) {
-            return [[away_abbrev, OTW], [home_abbrev, OTL]];
-        } else {
-            return [[home_abbrev, OTW], [away_abbrev, OTL]];
-        }
-    }
-}
-
-function detOtherGame(x, rw, otw, otl, l) {
+function detGame(x, rw, otw, otl, l) {
     const RW = rw;
     const OTW = otw;
     const OTL = otl;
@@ -111,21 +88,21 @@ function detOtherGame(x, rw, otw, otl, l) {
 
     if (wintype == "REG") {
         if (away_score > home_score) {
-            return [[away_abbrev, RW], [home_abbrev, L]];
+            return [[away_abbrev, [RW, 1, 0, 0, 0]], [home_abbrev, [L, 0, 0, 0, 1]]];
         } else {
-            return [[home_abbrev, RW], [away_abbrev, L]];
+            return [[home_abbrev, [RW, 1, 0, 0, 0]], [away_abbrev, [L, 0, 0, 0, 1]]];
         }
     } else {
         if (away_score > home_score) {
-            return [[away_abbrev, OTW], [home_abbrev, OTL]];
+            return [[away_abbrev, [OTW, 0, 1, 0, 0]], [home_abbrev, [OTL, 0, 0, 1, 0]]];
         } else {
-            return [[home_abbrev, OTW], [away_abbrev, OTL]];
+            return [[home_abbrev, [OTW, 0, 1, 0, 0]], [away_abbrev, [OTL, 0, 0, 1, 0]]];
         }
     }
 }
 
-const lbd = new Standings(detGame, "ranking");
-var lbdTwo = new Standings(a => detOtherGame(a, 3, 2, 1, 0), "otherranking");
+const lbd = new Standings(a => detGame(a, 2, 2, 1, 0), "ranking");
+var lbdTwo = new Standings(a => detGame(a, 3, 2, 1, 0), "otherranking");
 
 lbd.showStandings();
 lbdTwo.showStandings();
@@ -136,10 +113,8 @@ export function runStandings() {
     var otl = parseInt(document.getElementById("OTL").value);
     var l = parseInt(document.getElementById("L").value);
 
-    var lbdTwo = new Standings(a => detOtherGame(a, w, otw, otl, l), "otherranking");
+    var lbdTwo = new Standings(a => detGame(a, w, otw, otl, l), "otherranking");
     lbdTwo.updateStandings();
-
-    console.log(document.getElementById("RW").value);
 }
 
 var button = document.getElementById("runstandings");
